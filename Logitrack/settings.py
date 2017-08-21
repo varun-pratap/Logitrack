c974e9e1280acb3c870889cd1d7162888400bb15
varun-pratap <varunsingh8055@gmail.com>
2017-08-21 22:41:04 +0530

    My first model

--- Logitrack/settings.py
+++ Logitrack/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'userprofile',
 ]
 
 MIDDLEWARE = [
@@ -72,14 +73,22 @@ WSGI_APPLICATION = 'Logitrack.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'db.cnf',
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- userprofile/models.py
+++ userprofile/models.py
@@ -1,6 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth import User
 from django.db import models
 
+
+
 # Create your models here.
+class UserProfile(models.Model):
+	DEPARTMENT = (
+		('IT','Information Technology'),
+		('Support','Support'),
+		('OPPS','Operations'),
+	)
+	ROLES= (
+		('1','MANAGER'),
+		('2','SUPER USER'),
+		('3','ADMIN'),
+		('4','HUB INCHARGE'),
+		('5','DC INCHARGE'),
+		('6','Executives'),
+		('7','Leads'),
+	)
+	user = models.OneToOneField(User)
+	fname = models.CharField(max_length="20")
+	lname = models.CharField(max_length="20")
+	email = models.EmailField()
+	employeeID = models.CharField(max_length="10")
+	department = models.CharField(max_length="5",choices = DEPARTMENT)
+	doj = models.DateField(auto_now = True)
+	status = models.IntegerField()
+	role = models.CharField(max_length="15",choices = ROLES)
